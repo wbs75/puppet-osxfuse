1cb70da17618c94bf9f174995ebf01022c3238b7
wbs75 <wbs75@me.com>
2014-06-18 10:59:40 -0600

    Update osxfuse_spec.rb

--- spec/classes/osxfuse_spec.rb
+++ spec/classes/osxfuse_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'osxfuse' do
   it do
     should contain_package('OSXFuse').with({
-      :source   => 'http://downloads.sourceforge.net/project/osxfuse/osxfuse-2.6.4/osxfuse-2.6.4.dmg',
+      :source   => 'http://downloads.sourceforge.net/project/osxfuse/osxfuse-2.7.0/osxfuse-2.7.0.dmg',
       :provider => 'pkgdmg'
     })
   end
